fec940f4f2f5e18ec25cd4e2ebd47bd43b00b9ae
Sheiny Fabre <sheiny.fabre@gmail.com>
2018-09-21 08:23:31 -0300

    Improve polybar search for monitor, add transparency to i3lock

--- .config/polybar/bar.sh
+++ .config/polybar/bar.sh
@@ -7,4 +7,11 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar white &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload white &
+  done
+else
+  polybar --reload white &
+fi
+
